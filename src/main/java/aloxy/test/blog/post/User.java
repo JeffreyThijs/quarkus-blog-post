5d430afbaf3ab38736e8b2b1ef1295b4084a965c
JeffreyThijs <jeffrey.thijs@hotmail.com>
2021-03-25 10:35:45 +0100

    initual user/user resource

---
+++ src/main/java/aloxy/test/blog/post/User.java
@@ -0,0 +1,117 @@
+package aloxy.test.blog.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Table(name = "known_users")
+public class User extends PanacheEntity {
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @JsonIgnore
+    private boolean confirmed;
+
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, boolean confirmed) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.confirmed = confirmed;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isConfirmed() {
+        return this.confirmed;
+    }
+
+    public boolean getConfirmed() {
+        return this.confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public User username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public User password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    public User email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public User confirmed(boolean confirmed) {
+        setConfirmed(confirmed);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " username='" + getUsername() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", confirmed='" + isConfirmed() + "'" +
+            "}";
+    }
+
+
+    public static User findByUsername(String username){
+        return find("username", username).firstResult();
+    }
+
+    public boolean checkPassword(String password) {
+        return true;
+    }
+}
--- src/main/java/aloxy/test/blog/post/UserResource.java
+++ src/main/java/aloxy/test/blog/post/UserResource.java
@@ -1,16 +1,51 @@
 package aloxy.test.blog.post;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/hello-resteasy")
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public List<User> getAllUsers(){
+        return User.listAll();
     }
+
+    @POST
+    @Path("register")
+    @Transactional
+    public Response register(@Valid User user) {
+        user.id = null;
+        user.setConfirmed(true);
+        user.persist();
+        return Response.ok(user).status(201).build();
+    }
+
+    @POST
+    @Path("/login")
+    public Response login(@Valid User user) {
+        User userDB = User.findByUsername(user.getUsername());
+        if (userDB == null) {
+            return Response.ok("No user found").status(404).build();
+        }
+
+        if (!userDB.checkPassword(user.getPassword())) {
+            return Response.ok("Invalid credentials").status(403).build();
+        }
+
+        return Response.ok(user).status(201).build();
+
+    }
+
 }
